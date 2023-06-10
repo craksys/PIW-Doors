becb19f15954be07d41324a11f415e1ca6e7db9e
Kamil K <skkf0004@gmail.com>
2023-06-10 17:00:53 +0200

    add room, delete, access, and view for user

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,55 @@
 class RoomsController < ApplicationController
 before_action :require_login
   before_action :authorize_admin, only: [:new, :edit, :create, :update, :destroy]
+  #def room_params
+ # params.require(:room).permit(:name)
+#end
+def room_params
+    params.require(:room).permit(:name)
+    params.require(:room).permit(user_ids: [])
+  end
+
+def new
+    @room = Room.new
+  end
 def index
     @rooms = Room.all
   end
+def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      redirect_to @room
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @room = Room.find(params[:id])
+end
+
+def update
+    @room = Room.find(params[:id])
+
+    if @room.update(room_params)
+      redirect_to @room
+    else
+      render 'edit'
+    end
+  end
+  
+def destroy
+  @room = Room.find(params[:id])
+  @room.destroy
+  redirect_to rooms_path, notice: "Pokój został usunięty."
+end
+
+def edit
+  @room = Room.find(params[:id])
+end
+
+
 def authorize_admin
   if current_user.nil? || !current_user.admin?
     flash[:alert] = "Musisz być administratorem aby uzyskać dostęp"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :users
   resources :rooms
+  
   root to: 'rooms#index'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+  
 end
