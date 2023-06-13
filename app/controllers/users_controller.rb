95a6f14ff9e091a4a0b2dd0f815d5a9e01639f06
Kacper Wróblewski <85016688+kacper651@users.noreply.github.com>
2023-06-13 13:39:39 +0200

    Kosmetyka czy cos (#2)
    * Add bootstrap styles
    
    * added styles to all html files
    
    //TODO: add navbar to logout
    
    * added revoke permissions for user and implemented some styles and navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,34 @@
 class UsersController < ApplicationController
-def new
-  @user = User.new
-end
+  
+  def new
+    @user = User.new
+  end
 
-def create
-  existing_user = User.find_by(email: user_registration_params[:email])
-  if existing_user
-    flash[:error] = "Użytkownik o podanym adresie e-mail już istnieje."
-    redirect_to new_user_path
-  else
-    @user = User.new(user_registration_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path
+  def create
+    existing_user = User.find_by(email: user_registration_params[:email])
+    if existing_user
+      flash[:error] = "Użytkownik o podanym adresie e-mail już istnieje."
+      redirect_to new_user_path
     else
-      render :new
+      @user = User.new(user_registration_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        render :new
+      end
     end
   end
-end
 
-private
-def user_registration_params
-  base_params = params.require(:user).permit(:email)
-  password = params.require(:user).permit(:password)
-  base_params.merge(password: password[:password])
-end
+  private
 
-def user_params
-params.require(:user).permit(:email, :password, :password_confirmation)
-end
+  def user_registration_params
+    base_params = params.require(:user).permit(:email)
+    password = params.require(:user).permit(:password)
+    base_params.merge(password: password[:password])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
